c917935d7bbe6b7984c767abd7e044dafd9523a5
cawzkf <camilafelixdosreis@gmail.com>
2024-09-08 18:10:08 -0400

    Instalação: Npm install e  bootstrap; Classe cadastros

---
+++ filtrer.js
@@ -0,0 +1,20 @@
+const cadastros = [
+    {
+        "nome":"Person1",
+        "age": 20
+    },
+    {
+        "nome": "Person2",
+        "age":8
+    },
+    {
+        "nome": "Person3",
+        "age":32
+    },
+    {
+        "nome": "Person4",
+        "age":53
+    }
+];  
+
+
